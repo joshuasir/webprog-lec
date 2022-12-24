4d6e1d6419e2f920720638dca5e2b38cb740718c
Antony <mahiroyasaka9033@gmail.com>
2022-12-24 16:05:08 +0700

    Detail
    bagian detail

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -23,13 +23,12 @@
     <div class="title">
       <h3>{{$product->name}}</h3>
     </div>
+    <div style="color: #A9A9A9; font-size: 1.5rem">
+      <p>Rp. {{$product->price}}</p>
+    </div>
+    <br>
     <div class="more">
-      <h5>Category:</h5>
-      <p style="font-size: 15px">{{$product->category}}</p>
-      <h5>Price:</h5>
-      <p style="font-size: 15px">Rp. {{$product->price}}</p>
-      <h5>Description:</h5>
-      <p style="font-size: 15px">{{$product->description}}</p>
+      <p>{{$product->description}}</p>
     </div>
 
   @if(!(Session::get('user')))
@@ -54,12 +53,10 @@
       @csrf
 
         <input type="hidden" name="id" value="{{$product->id}}">
-        <label for="quantity" style="font-size: 20px">
-          Quantity:
-        </label>
-        <input class="form-control mb-2 @error('quantity') is-invalid  @enderror" type="number" name="quantity" value="{{old('quantity')}}" style="font-size: 15px">
+        
+        <input class="form-control mb-2 @error('quantity') is-invalid  @enderror" type="number" name="quantity" min="1" max="100" value="{{old('quantity')}}" style="font-size: 15px; width: 5em">
 
-      <button class="btn btn-warning btn-sm" type="submit" style="width: 145px">
+      <button class="btn btn-warning btn-sm" type="submit" style="width: 25em">
         <h5>
           Add to Cart
         </h5>
