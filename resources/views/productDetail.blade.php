d565142717e4422370a0b42bda3a88052138d394
Antony <mahiroyasaka9033@gmail.com>
2022-12-24 17:22:50 +0700

    Detail
    tambahan push detail page

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -8,8 +8,7 @@
 
 @section('content')
 @if($product)
-<div class="container">
-
+<div class="product_box">
   <div class="img-box">
     @if (Storage::disk('public')->exists($product->image))
       <img src="{{Storage::url($product->image)}}" alt="product-image">
@@ -17,71 +16,72 @@
       <img src="{{$product->image}}" alt="product-image">
     @endif
   </div>
-
-
-  <div class="info">
-    <div class="title">
-      <h3>{{$product->name}}</h3>
-    </div>
-    <div style="color: #A9A9A9; font-size: 1.5rem">
-      <p>Rp. {{$product->price}}</p>
-    </div>
-    <br>
-    <div class="more">
-      <p>{{$product->description}}</p>
-    </div>
-
-  @if(!(Session::get('user')))
-  <a href="/login">
-    <div class="btn btn-warning btn-sm">
-      Login to Buy
-    </div>
-  </a>
-  @endif
-
-  @if(Session::get('user') && Session::get('user')['role']==='admin')
-  <a href="/updateItem/{{$product->id}}">
-    <div class="btn btn-warning btn-sm">Update Item</div>
-  </a>
-  @endif
-
-  @if(Session::get('user') && Session::get('user')['role']==='customer')
-  <div class="form_quantity">
-
-
-    <form action="/addcart" method="post" class="qty-form ">
-      @csrf
-
-        <input type="hidden" name="id" value="{{$product->id}}">
+  <div class="container">
+    <div class="info">
+      <div class="title">
+        <h3>{{$product->name}}</h3>
+      </div>
+      <div class="price">
+        <p>Rp. {{$product->price}}</p>
+      </div>
+      <br>
+      <div class="more">
+        <p>{{$product->description}}</p>
+      </div>
+  
+    @if(!(Session::get('user')))
+    <a href="/login">
+      <div class="btn btn-warning btn-sm">
+        Login to Buy
+      </div>
+    </a>
+    @endif
+  
+    @if(Session::get('user') && Session::get('user')['role']==='admin')
+    <a href="/updateItem/{{$product->id}}">
+      <div class="btn btn-warning btn-sm">Update Item</div>
+    </a>
+    @endif
+  
+    @if(Session::get('user') && Session::get('user')['role']==='customer')
+    <div class="form_quantity">
+  
+  
+      <form action="/addcart" method="post" class="qty-form ">
+        @csrf
+  
+          <input type="hidden" name="id" value="{{$product->id}}">
+          
+          <input class="form-control mb-2 @error('quantity') is-invalid  @enderror" type="number" name="quantity" min="1" max="100" value="{{old('quantity')}}" style="font-size: 15px; width: 5em">
+  
+        <button class="btn btn-warning btn-sm" type="submit" style="width: 25em; height: 6em;">
+          <h5>
+            Add to Cart
+          </h5>
+        </button>
+  
+        @error('quantity')
         
-        <input class="form-control mb-2 @error('quantity') is-invalid  @enderror" type="number" name="quantity" min="1" max="100" value="{{old('quantity')}}" style="font-size: 15px; width: 5em">
-
-      <button class="btn btn-warning btn-sm" type="submit" style="width: 25em">
-        <h5>
-          Add to Cart
-        </h5>
-      </button>
-
-      @error('quantity')
-      
-        <small class="error-message">
-          {{$message}}
-        </small>
-      @enderror
-      @if(session()->has('success'))
-        <div class="alert alert-success mt-4">
-            {{ session()->get('success') }}
-        </div>
-    @endif 
-      
-    </form>
+          <small class="error-message">
+            {{$message}}
+          </small>
+        @enderror
+        @if(session()->has('success'))
+          <div class="alert alert-success mt-4">
+              {{ session()->get('success') }}
+          </div>
+      @endif 
+        
+      </form>
+    </div>
+    </div>
+    @endif
+  
+  
+  
   </div>
-  </div>
-  @endif
-
-
-
 </div>
+
 @else
 <div class="h1">
   Product Not Found!
